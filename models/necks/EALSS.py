896d1145e71eff51fd36ede15ad845cc39da8ce7
haoxuanhan1999 <127754458+haoxuanhan1999@users.noreply.github.com>
2024-12-30 10:24:57 +0800

    Add files via upload

--- models/necks/EALSS.py
+++ models/necks/EALSS.py
@@ -22,10 +22,10 @@ from .cam_stream_lss import LiftSplatShoot
 @DETECTORS.register_module()
 class EALSS_CAM(BaseModule):
     """Base class of Multi-modality VoxelNet."""
-    def __init__(self, lss=False, lc_fusion=False, camera_stream=False,
+    def __init__(self, lss=False, lc_fusion=False, camera_stream=True,
                 camera_depth_range=[4.0, 45.0, 1.0], img_depth_loss_weight=1.0,  img_depth_loss_method='kld',
-                grid=0.6, num_views=6, se=False, final_dim=(900, 1600), pc_range=[-50, -50, -5, 50, 50, 3],
-                downsample=4, imc=512, lic=384, step=7, **kwargs):
+                grid=0.8, num_views=6, se=False, final_dim=(900, 1600), pc_range=[-51.2, -51.2, -5.0, 51.2, 51.2, 3.0],
+                downsample=16, imc=512, lic=384, step=7, **kwargs):
 
         super(EALSS_CAM, self).__init__(**kwargs)
         self.num_views = num_views
@@ -43,11 +43,13 @@ class EALSS_CAM(BaseModule):
         
 
 
-    def extract_feat(self, points, x, input):
+    def extract_feat(self, points, x, input, img_metas):
         
         B, S, N, C, H, W = x.shape  #获取图像特征维度参数
-        lidar2img_rt = input       #获取lidar2img矩阵 [1,3,6,4,4]
+        lidar2img_rt = input[8]       #获取lidar2img矩阵 [1,3,6,4,4]
         lidar2img_rt = lidar2img_rt.squeeze(0)  #[3,6,4,4]
+        post_rots = input[4].squeeze(0)         #[3,6,3,3]
+        post_trans = input[5].squeeze(0)        #[3,6,3]
 
         rots = []  #lidar2img的rot
         trans = []  #lidar2img的trans
@@ -73,46 +75,61 @@ class EALSS_CAM(BaseModule):
         
 
         batch_size = len(points)  #遍历点云的时间数
-        depth = torch.zeros(batch_size, img_size[1], 1, img_size[3], img_size[4]).cuda() # 创建大小
+        depth = torch.zeros(batch_size, 6, 1, 256, 704).cuda() # 创建大小,与图像一致
         for b in range(batch_size):
-            cur_coords = points[b].float()[:, :3]  #取点的xyz
+            cur_coords = points[b].float()[:, :, :3]  #取点的xyz
+            cur_coords = cur_coords.squeeze(0)  #移除点云中的batchsize维度
             # lidar2image
             cur_coords = rots_depth[b].matmul(cur_coords.transpose(1, 0))
-            cur_coords += trans_depth[b].reshape(-1, 3, 1)
+            cur_coords += trans_depth[b].reshape(-1, 3, 1)  #[6, 3, num_points]
 
             # get 2d coords
-            dist = cur_coords[:, 2, :]
-            cur_coords[:, 2, :] = torch.clamp(cur_coords[:, 2, :], 1e-4, 1e4)
-            cur_coords[:, :2, :] /= cur_coords[:, 2:3, :]
+            dist = cur_coords[:, 2, :]  #获取深度Z轴
+            cur_coords[:, 2, :] = torch.clamp(cur_coords[:, 2, :], 1e-4, 1e4)  #限制深度范围
+            cur_coords[:, :2, :] /= cur_coords[:, 2:3, :]  #归一化，将点投影到图像坐标
+            cur_coords = cur_coords[:, :2, :].transpose(1, 2)  #生成图像坐标[6,num_points,2]
+
+
             # imgaug
+            ones = torch.ones((cur_coords.shape[0], cur_coords.shape[1], 1), device=cur_coords.device)
+            cur_coords_homogeneous = torch.cat([cur_coords, ones], dim=-1)  # [6, num_points, 3]
+            # 应用增强后的旋转和平移
+            post_rots_expanded = post_rots[b] # [6, 3, 3]
+            post_trans_expanded = post_trans[b]  # [6, 3]
+            transformed_coords = (
+                post_rots_expanded.matmul(cur_coords_homogeneous.transpose(2, 1)).transpose(2, 1)  # 旋转
+                + post_trans_expanded.unsqueeze(1)  # 平移
+            )
 
-            cur_coords = cur_coords[:, :2, :].transpose(1, 2)
+            cur_coords = transformed_coords[..., :2]  # 更新后的图像坐标
+            
             # normalize coords for grid sample
-            cur_coords = cur_coords[..., [1, 0]]
+            cur_coords = cur_coords[..., [1, 0]]  #生成图像坐标[6,num_points,2]
 
+            #过滤有效点
             on_img = (
-                (cur_coords[..., 0] < img_size[3])
+                (cur_coords[..., 0] < 256)
                 & (cur_coords[..., 0] >= 0)
-                & (cur_coords[..., 1] < img_size[4])
+                & (cur_coords[..., 1] < 704)
                 & (cur_coords[..., 1] >= 0)
             )
 
-            for c in range(on_img.shape[0]):
-                masked_coords = cur_coords[c, on_img[c]].long()  # 点云投影到图像坐标
-                masked_dist = dist[c, on_img[c]]  # 对应深度
-                depth[b, c, 0, masked_coords[:, 0], masked_coords[:, 1]] = masked_dist.float()  # 稀疏的深度约束图（用于计算loss） 1, 6, 1, 448, 800
+            for c in range(on_img.shape[0]):  #遍历摄像头
+                masked_coords = cur_coords[c, on_img[c]].long()  # 提取第C个视角下投影到图像的点云的图像坐标
+                masked_dist = dist[c, on_img[c]]  # 获取有效点深度
+                depth[b, c, 0, masked_coords[:, 0], masked_coords[:, 1]] = masked_dist.float()  # 每个投影点的深度值填入深度图对应位置
 
-        step = 7
-        B, N, C, H, W = depth.size()
+        step = 7  #滑动窗口大小
+        B, N, C, H, W = depth.size()  #深度图张量维度
         depth_tmp = depth.reshape(B*N, C, H, W)
-        pad = int((step - 1) // 2)
-        depth_tmp = F.pad(depth_tmp, [pad, pad, pad, pad], mode='constant', value=0)
+        pad = int((step - 1) // 2)  #填充大小
+        depth_tmp = F.pad(depth_tmp, [pad, pad, pad, pad], mode='constant', value=0)  #为每个深度图添加0填充
         patches = depth_tmp.unfold(dimension=2, size=step, step=1)
         patches = patches.unfold(dimension=3, size=step, step=1)
-        max_depth, _ = patches.reshape(B, N, C, H, W, -1).max(dim=-1)  # [2, 6, 1, 256, 704]
+        max_depth, _ = patches.reshape(B, N, C, H, W, -1).max(dim=-1)  # [3, 6, 1, 256, 704]生成与深度图分辨率相同的最大深度图
         img_metas[0].update({'max_depth': max_depth})
 
-        # 求解max_depth四个方向梯度, 随后concat depth, 以缓解深度跳变对深度预测模块的影响
+        # 求解max_depth四个方向梯度, 随后concat depth, 以缓解深度跳变对深度预测模块的影响(梯度特征提供了深度图的局部变化信息，对建模边缘特性或局部特征非常有用)
         step = float(step)
         shift_list = [[step / H, 0.0 / W], [-step / H, 0.0 / W], [0.0 / H, step / W], [0.0 / H, -step / W]]
         max_depth_tmp = max_depth.reshape(B*N, C, H, W)
@@ -125,13 +142,13 @@ class EALSS_CAM(BaseModule):
             output_mask = ((output == max_depth) == False)
             output = output * output_mask
             output_list.append(output)
-        grad = torch.cat(output_list, dim=2)  # [2, 6, 4, 256, 704]
+        grad = torch.cat(output_list, dim=2)  
         max_grad = torch.abs(grad).max(dim=2)[0].unsqueeze(2)
-        img_metas[0].update({'max_grad': max_grad})
+        img_metas[0].update({'max_grad': max_grad})  #最大梯度图
         #depth_ = depth
-        depth = torch.cat([depth, grad], dim=2)  # [1, 6, 5, 448, 800]
+        depth = torch.cat([depth, grad], dim=2)  #[3,6,5,256,704]
 
-        img_bev_feat, depth_dist, img_metas = self.lift_splat_shot_vis(img_feats_view, rots, trans, lidar2img_rt=lidar2img_rt, img_metas=img_metas
+        img_bev_feat, depth_dist, img_metas = self.lift_splat_shot_vis(x, rots, trans, lidar2img_rt=lidar2img_rt, img_metas=img_metas
                                                             ,depth_lidar=depth)
         
         return img_bev_feat
@@ -140,6 +157,7 @@ class EALSS_CAM(BaseModule):
     def forward(self,
                 points=None,
                 img_feats=None,
+                img=None,
                 img_metas=None,
                 ):
         """Forward training function.
@@ -168,6 +186,6 @@ class EALSS_CAM(BaseModule):
             dict: Losses of different branches.
         """
         img_bev_feats = self.extract_feat(
-            points, x=img_feats, input=img_metas)
+            points, x=img_feats, input=img, img_metas=img_metas)
         
         return img_bev_feats
